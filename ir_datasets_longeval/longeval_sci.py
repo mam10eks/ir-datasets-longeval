6ee2fefe169df34bea1e4b350d7126db7f893d67
Maik Fröbe <maik.froebe@uni-jena.de>
2025-03-21 11:14:10 +0100

    the abstract can be missing

--- ir_datasets_longeval/longeval_sci.py
+++ ir_datasets_longeval/longeval_sci.py
@@ -53,7 +53,10 @@ class LongEvalSciDoc(NamedTuple):
     updatedDate: str
 
     def default_text(self):
-        return self.title + " " + self.abstract
+        ret = self.title
+        if self.abstract is not None:
+            ret += " " + self.abstract
+        return ret
 
 class ExtractedPath():
     def __init__(self, path):
--- tests/test_official_datasets.py
+++ tests/test_official_datasets.py
@@ -5,9 +5,7 @@ class TestOfficialDatasets(unittest.TestCase):
     def test_sci_dataset(self):
         dataset = load("longeval-sci/2024-11/train")
 
-
         expected_queries = {"ce5bfacf-8652-4bc1-a5b0-6144a917fb1c": "streptomyces"}
-        expected_qrels = [{'doc_id': '140120179', 'query_id': '1234-1234-1234-1234-1234', 'rel': 2}]
 
         self.assertIsNotNone(dataset)
         example_doc = dataset.docs_iter().__next__()
@@ -23,3 +21,10 @@ class TestOfficialDatasets(unittest.TestCase):
         self.assertEqual([], dataset.get_past_datasets())
         docs_store = dataset.docs_store()
         self.assertEqual("11699524", docs_store.get("11699524").doc_id)
+        self.assertIn("level of customers", docs_store.get("11699524").default_text())
+
+    def test_sci_default_text_for_document_without_abstract(self):
+        dataset = load("longeval-sci/2024-11/train")
+
+        docs_store = dataset.docs_store()
+        self.assertEqual("Migration, Social Norms and Experiences of Female Genital Mutilation (FGM) among Somali Communities in the UK", docs_store.get("154684342").default_text())
